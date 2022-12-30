fefddab468f78ae642c02e14ac21460e312b0ffe
Ajit Patel <80194170+ajitpatel28@users.noreply.github.com>
2022-12-30 21:14:28 +0530

    Update likes.js

--- routes/likes.js
+++ routes/likes.js
@@ -3,7 +3,6 @@ const controllers = require('../controllers/likes');
 const router = Router();
 
 router.get('/likes', controllers.getAllLikes);
-router.get('/likes/:id', controllers.getLikeById);
 router.post('/likes/', controllers.createLike);
 router.put('/likes/:id', controllers.updateLike);
 router.delete('/likes/:id', controllers.deleteLike);
